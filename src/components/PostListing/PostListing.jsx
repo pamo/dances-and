d363793e9700ab7c339c64916f34ee35a4d45da7
Pamela Ocampo <pamela.ocampo@gmail.com>
2019-02-17 09:48:27 -0800

    A collection

--- src/components/PostListing/PostListing.jsx
+++ src/components/PostListing/PostListing.jsx
@@ -1,5 +1,15 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'tachyons-components';
+
+const Container = styled('section')`cf w-100 pa2-ns`;
+const Post = styled('article')`fl w-100 w-50-m  w-25-ns pa2-ns`;
+const PostCoverContainer = styled('div')`aspect-ratio aspect-ratio--1x1`;
+const PostCover = styled('img')`db bg-center cover aspect-ratio--object`;
+const PostTitleLink = styled('div')`ph2 ph0-ns pb3 link db`;
+const PostMainTitle = styled('h3')`f5 f4-ns mb0 black-90`;
+const PostSubTitle = styled('h3')`f6 f5 fw4 mt2 black-60`;
+// <PostCover style="background-image:url(http://mrmrs.github.io/images/0006.jpg)" />
 
 class PostListing extends React.Component {
   getPostList() {
@@ -10,6 +20,8 @@ class PostListing extends React.Component {
         tags: postEdge.node.frontmatter.tags,
         cover: postEdge.node.frontmatter.cover,
         title: postEdge.node.frontmatter.title,
+        artist: postEdge.node.frontmatter.artist,
+        venue: postEdge.node.frontmatter.venue,
         date: postEdge.node.fields.date,
         excerpt: postEdge.node.excerpt,
         timeToRead: postEdge.node.timeToRead
@@ -17,17 +29,22 @@ class PostListing extends React.Component {
     });
     return postList;
   }
+
   render() {
     const postList = this.getPostList();
     return (
-      <div>
-        {/* Your post list here. */
-        postList.map(post => (
-          <Link to={post.path} key={post.title}>
-            <h1>{post.title}</h1>
-          </Link>
+      <Container>
+        {postList.map(post => (
+          <Post key={post.title}>
+            <Link to={post.path}>
+              <PostTitleLink>
+                <PostMainTitle>{post.artist}</PostMainTitle>
+                <PostSubTitle>{post.venue}</PostSubTitle>
+              </PostTitleLink>
+            </Link>
+          </Post>
         ))}
-      </div>
+      </Container>
     );
   }
 }
