c7d8b1b79915f78217b58fb4c4fbcc8fa706dbd9
Pamela Ocampo <pamela.ocampo@gmail.com>
2019-02-19 11:46:25 -0800

    Fix bleeding on about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,74 +1,83 @@
 import React, { Component } from 'react';
 import './About.css';
+import styled from 'tachyons-components';
+
+const BodyLink = styled('a')`link underline hover-light-purple`;
 
 class About extends Component {
   render() {
     return (
-      <div className="flex flex-column vh-100 justify-center items-center">
-        <h1>
+      <div className="db w100 pa3 center mw5 mw7-ns">
+        <h1 className="measure tc">
           Live music gives me
           {' '}
-          <a href="https://en.wikipedia.org/wiki/Frisson" target="_blank" className="blue link dim">
+          <a
+            href="https://en.wikipedia.org/wiki/Frisson"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="blue link dim"
+          >
             frisson
           </a>
           .
         </h1>
-        <section className="mw5 mw7-ns center pa3 ph5-ns">
+        <section className="center measure">
           <p className="lh-copy measure">
-            Despite moving to the Bay Area in 2013, I truly didn't start taking advantage of the
-            live music scene until 2016 when I met
+            Despite moving to the Bay Area in 2013, I truly didn't start taking
+            advantage of the live music scene until 2016 when I met
             {' '}
-            <a
+            <BodyLink
               href="https://thisisimportant.net/2018/12/09/my-2018-year-in-music-data-analysis-and-insights/"
               target="_blank"
-              className="link underline hover-light-purple"
+              rel="noopener noreferrer"
             >
               Sarah
-            </a>
+            </BodyLink>
             , found the
             {' '}
-            <a
+            <BodyLink
               href="https://www.facebook.com/groups/concertraptor/"
               target="_blank"
-              className="link underline hover-light-purple"
+              rel="noopener noreferrer"
             >
               Concert Raptors
-            </a>
+            </BodyLink>
             {' '}
-            group, and started a job that would allow me to better plan where I would be in the
-            world months ahead of time.
+            group, and started a job that would allow me to better plan where I
+            would be in the world months ahead of time.
           </p>
           <p className="lh-copy measure">
-            Through these individuals I've become better connected with the music scene and myself
-            by seeking out the pleasure that comes with experiencing music live with a concert buddy
-            or two or myself.
+            Through these individuals I've become better connected with the
+            music scene and myself by seeking out the pleasure that comes with
+            experiencing music live with a concert buddy or two or myself.
           </p>
           <p className="lh-copy measure">
-            With every new hobby comes a curiosity to see metrics and data at a glance and albeit
-            Last.fm can provide a almost complete picture of my listening habits, historical
-            artifacts of the shows I've been were spread among ticket stubs, Foursquare check-ins,
-            and vague memories of dancing and light shows. Inspired by
+            With every new hobby comes a curiosity to see metrics and data at a
+            glance and albeit Last.fm can provide a almost complete picture of
+            my listening habits, historical artifacts of the shows I've been
+            were spread among ticket stubs, Foursquare check-ins, and vague
+            memories of dancing and light shows. Inspired by
             {' '}
-            <a
+            <BodyLink
               href="https://tinnitus.robweychert.com/"
               target="_blank"
-              className="link underline hover-light-purple"
+              rel="noopener noreferrer"
             >
               Rob Weychert
-            </a>
-            , I sought out to create a blog to paint a picture of this data for myself and anyone
-            else to see.
+            </BodyLink>
+            , I sought out to create a blog to paint a picture of this data for
+            myself and anyone else to see.
           </p>
           <p className="lh-copy measure">
             Here's to more dancing, coffee, and
             {' '}
-            <a
+            <BodyLink
               href="http://www.flask.com/the-art-of-the-disco-nap-sleeping-between-happy-hour-and-a-night-out/"
               target="_blank"
-              className="link underline hover-light-purple"
+              rel="noopener noreferrer"
             >
               disco naps
-            </a>
+            </BodyLink>
             {' '}
             in 2019 and beyond!
           </p>
